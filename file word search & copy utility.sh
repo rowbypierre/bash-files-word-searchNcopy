2d6265863bb5fffe7e0110ca7a7308d498242853
rowbypierre <rowbypierre1998@gmail.com>
2023-12-20 20:31:36 -0500

    rename bash file, spell check all files, create detailed readme markdown file

--- file word search & copy utility.sh
+++ file word search & copy utility.sh
@@ -2,20 +2,19 @@
 
 echo"
 This file will do the following:
-	1. Read and store keyword from user
-	2. Read and store folder name from user
+	1. Read and store keyword from user.
+	2. Read and store folder name from user.
 		2-a. Determine if input provided exist in current working directory.
 		2-b-1. Create folder if folder does not exist in current directory.
-		2-b-2-c. Use existing folder to copy files.
-	3. Search keyword against all files recursively
-	4. Files containing keyword will be copied to folder (2)
-	5. Line and line numbers will append to copied file (5)  "
+		2-b-2. Use existing folder to copy files.
+	3. Search keyword against all files recursively.
+	4. Files containing keyword will be copied to folder (2).
+	5. Line and line numbers will append to copied file (5).  "
 echo
 read -p "Enter keyword to search in files: " key
 echo
 echo "Files will be seached for: $key"
 echo
-
 read -p "Enter folder to copy files containing keyword: " cpfolder
 echo
 if [ ! -d "$cpfolder" ] ; then
@@ -30,15 +29,13 @@ else
 fi
 
 DirS=$( find . -type d )
-for dir in $DirS ;
-do
+for dir in $DirS ; do
 
 	if [ "$dir" != $cpfolder ] ; then
 
 		echo "*******************FOLDER: $dir***********************"
 
-		for file in "$dir"/* ;
-		do 
+		for file in "$dir"/* ; do 
 			if [ -f "$file" ] ; then
 				echo "File: $file scanning.. "
 				echo
@@ -47,7 +44,7 @@ do
 
 				if [ ! -z $checker ] ; then
 					fileN=$( basename $file )
-					echo "$fileN HAS keyoword"
+					echo "$fileN HAS keyword"
 					echo
 
 					cp "$file" "$cpfolder/$fileN"
